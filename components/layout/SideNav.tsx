a009610306d4ea6e32f7c12c3245f4e43bd52c03
Mostafa <mostafamabdallah94@gmail.com>
2024-01-28 16:23:40 +0200

    change logo extention name

--- components/layout/SideNav.tsx
+++ components/layout/SideNav.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { memo } from "react";
-import logo from "../../public/logo.png";
+import logo from "../../public/logo.PNG";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faGrip,
