c327c6fb0f29093a0d56cabd7db8b8c46e920654
Mostafa <mostafamabdallah94@gmail.com>
2024-03-01 16:00:30 +0200

    solve connection problem

--- utilities/fetch.tsx
+++ utilities/fetch.tsx
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+const BASE_URL = process.env.BASE_URL || "https://taswika-72f10gyk3-mostafas-projects-4ce3ab21.vercel.app";
+
+console.log(BASE_URL);
+
+
 export const customFetch = axios.create({
-  baseURL: process.env.BASE_URL+"/api/",
+  baseURL:BASE_URL+"/api/",
 });
