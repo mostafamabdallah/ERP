61fead4c229fc78da48f077d410185a0da1231b2
Mostafa <mostafamabdallah94@gmail.com>
2024-03-09 21:58:43 +0200

    test

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
                 source: "/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Origin", value: "*" },
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
@@ -17,9 +17,9 @@ const nextConfig = {
         ]
     },
     reactStrictMode: true,
-    env: {
-      BASE_URL: process.env.BASE_URL,
-    }
+    // env: {
+    //   BASE_URL: process.env.BASE_URL,
+    // }
 }
 
 module.exports = nextConfig
