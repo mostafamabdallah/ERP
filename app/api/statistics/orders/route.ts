679561ed16910cbc4d1bf52645c94517e4416428
Mostafa <mostafamabdallah94@gmail.com>
2024-05-17 18:42:34 +0300

    solve statistics query

--- app/api/statistics/orders/route.ts
+++ app/api/statistics/orders/route.ts
@@ -5,10 +5,21 @@ const prisma = new PrismaClient();
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  
   try {
     let ordersPerDay = await prisma.$queryRawUnsafe(
-      'SELECT CAST(DATE(create_at) AS CHAR) AS day, CAST(COUNT(*) AS CHAR) AS orderCount FROM "Order" GROUP BY day;'
+      `SELECT
+          DATE("create_at") AS order_date,
+          COUNT(*) AS order_count
+      FROM
+          "Order"
+      GROUP BY
+          DATE("create_at")
+      ORDER BY
+          order_date;`
     );
+  console.log(ordersPerDay);
+
 
     return NextResponse.json({ ordersPerDay: ordersPerDay });
   } catch (error) {
