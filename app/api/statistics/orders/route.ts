2b12f2930beedd60cfba96fdced75f8f292a3524
Mostafa <mostafamabdallah94@gmail.com>
2024-05-18 21:30:01 +0300

    solve indentation of the statistic file

--- app/api/statistics/orders/route.ts
+++ app/api/statistics/orders/route.ts
@@ -5,21 +5,10 @@ const prisma = new PrismaClient();
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  
   try {
     let ordersPerDay = await prisma.$queryRawUnsafe(
-      `SELECT
-          DATE("create_at") AS order_date,
-          COUNT(*) AS order_count
-      FROM
-          "Order"
-      GROUP BY
-          DATE("create_at")
-      ORDER BY
-          order_date;`
+      `SELECT DATE("create_at") AS order_date, COUNT(*) AS order_count FROM "Order" GROUP BY DATE("create_at") ORDER BY order_date;`
     );
-  console.log(ordersPerDay);
-
 
     return NextResponse.json({ ordersPerDay: ordersPerDay });
   } catch (error) {
