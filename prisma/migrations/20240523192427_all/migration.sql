6cc6dea4ce8c54e4d7927cf0f73eccdc6d7ade31
Mostafa <mostafamabdallah94@gmail.com>
2024-05-23 22:34:20 +0300

    push migration

---
+++ prisma/migrations/20240523192427_all/migration.sql
@@ -0,0 +1,109 @@
+-- CreateTable
+CREATE TABLE "Customer" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "address" VARCHAR(255) NOT NULL,
+    "phone" VARCHAR(255) NOT NULL,
+    "status" VARCHAR(20) NOT NULL,
+    "type" VARCHAR(20),
+    "create_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Customer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" SERIAL NOT NULL,
+    "orderNumber" TEXT NOT NULL,
+    "customerId" INTEGER NOT NULL,
+    "deliveryCost" DOUBLE PRECISION NOT NULL,
+    "status" TEXT NOT NULL,
+    "create_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Item" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "quantity" DOUBLE PRECISION NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "unit" TEXT NOT NULL,
+    CONSTRAINT "Item_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "itemId" INTEGER,
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ItemOrder" (
+    "orderId" INTEGER NOT NULL,
+    "itemId" INTEGER NOT NULL,
+    "quantity" DOUBLE PRECISION NOT NULL,
+    CONSTRAINT "ItemOrder_pkey" PRIMARY KEY ("orderId", "itemId")
+);
+
+-- CreateTable
+CREATE TABLE "_CategoryToItem" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "DeliveryMan" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "phone" VARCHAR(20) NOT NULL,
+    "latitude" DOUBLE PRECISION NOT NULL,
+    "longitude" DOUBLE PRECISION NOT NULL,
+    CONSTRAINT "DeliveryMan_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Customer_phone_key" ON "Customer"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Item_name_key" ON "Item"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CategoryToItem_AB_unique" ON "_CategoryToItem"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CategoryToItem_B_index" ON "_CategoryToItem"("B");
+
+-- AddForeignKey
+ALTER TABLE
+    "Order"
+ADD
+    CONSTRAINT "Order_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE
+    "ItemOrder"
+ADD
+    CONSTRAINT "ItemOrder_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "Item"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE
+    "ItemOrder"
+ADD
+    CONSTRAINT "ItemOrder_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE
+    "_CategoryToItem"
+ADD
+    CONSTRAINT "_CategoryToItem_A_fkey" FOREIGN KEY ("A") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE
+    "_CategoryToItem"
+ADD
+    CONSTRAINT "_CategoryToItem_B_fkey" FOREIGN KEY ("B") REFERENCES "Item"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ prisma/migrations/20240523193000_adding_deliver_man_id_to_order_table/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "deliveryManId" INTEGER;
+
+
