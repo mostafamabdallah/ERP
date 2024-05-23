959bb15fbc7c4e553941b9345f71a0541ebda3a3
Mostafa <mostafamabdallah94@gmail.com>
2024-05-23 14:41:33 +0300

    shooting

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,3 +1,31 @@
+"use client";
+import React from "react";
+// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+// import "leaflet/dist/leaflet.css";
+// import L from "leaflet";
+import taswiqa from "../../public/icon.png";
+import deliveryMan from "../../public/delivaryman.png";
+
+// delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+// const taswiqaIcon = L.icon({
+//   iconUrl: taswiqa.src,
+//   iconSize: [25, 25],
+//   iconAnchor: [12, 41],
+//   popupAnchor: [1, -34],
+// });
+
+// const deliveryManIcon = L.icon({
+//   iconUrl: deliveryMan.src,
+//   iconSize: [50, 50],
+//   iconAnchor: [12, 41],
+//   popupAnchor: [1, -34],
+// });
+
+const position: [number, number] = [30.824350874212136, 30.53429040746672];
+const deliveryManPosition: [number, number] = [
+  30.82714802683165, 30.534317019077328,
+];
 
 const Page: React.FC = () => {
   return <></>;
